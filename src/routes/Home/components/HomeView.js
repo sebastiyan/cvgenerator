60bd51b344bc3dc240c1f205c56faac17a6f2dfb
Tanya Sebastiyan <tanyasebastiyan@Tanyas-MacBook-Pro.local>
2017-09-03 20:18:41 +0300

    Try to save as pdf

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
+import pdfConverter from 'jspdf';
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
-  </div>
-)
+class HomeView extends React.Component {
+  constructor(props){
+    super(props);
+    this.pdfToHTML=this.pdfToHTML.bind(this);
+  }
+
+  pdfToHTML(){
+    var pdf = new pdfConverter('p', 'pt', 'letter');
+    var source = '<div><center><h2>HTML to PDF</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing </p></center></div>'
+    var specialElementHandlers = {
+      '#bypassme': function(element, renderer) {
+        return true
+      }
+    };
+
+    var margins = {
+      top: 50,
+      left: 60,
+      width: 545
+    };
+
+    pdf.fromHTML (
+      source // HTML string or DOM elem ref.
+      , margins.left // x coord
+      , margins.top // y coord
+      , {
+          'width': margins.width // max width of content on PDF
+          , 'elementHandlers': specialElementHandlers
+        },
+      function (dispose) {
+        // dispose: object with X, Y of the last line add to the PDF
+        // this allow the insertion of new lines after html
+        pdf.save('html2pdf.pdf');
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div classID="HTMLtoPDF">
+          <center>
+            <h2>HTML to PDF</h2>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+          </center>
+        </div>
+        <button onClick={this.pdfToHTML}>Download PDF</button>
+      </div>
+    );
+  } 
+}
 
 export default HomeView
